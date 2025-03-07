e1c9ed43ddb32d647b045e5881110acabb084386
FoxowDV <65514843+FoxowDV@users.noreply.github.com>
2025-03-07 10:40:54 -0700

    cambio tamano

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from PySide6.QtCore import Qt
 class Window(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.setMinimumSize(900, 600)
+        self.setMaximumSize(900, 600)
 
 
 
